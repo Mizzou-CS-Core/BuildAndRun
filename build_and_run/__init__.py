8a97409a6e36569fdc00c07734d2280bf9ce45a3
Matt Marlow <thequantumblink@gmail.com>
2025-04-28 13:05:05 -0500

    feat: some fix ups

---
+++ build_and_run/__init__.py
@@ -0,0 +1,3 @@
+# CompileAndRun/__init__.py
+from .build_and_run import compile, run_executable  # whatever you expose
+__all__ = ["compile", "run_executable"]
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-  name="CompileAndRun",
-  version="1.0.0",
+  name="build_and_run",
+  version="1.1.0",
   packages=find_packages(),
 )
